c538c952d480a27969ea8007bf013c43397d386f
Danilo Silva <49529180+JDaniloC@users.noreply.github.com>
2022-03-31 23:57:41 -0300

    Add files via upload

---
+++ create-tables.sql
@@ -0,0 +1,143 @@
+DROP TABLE Centro;
+DROP TABLE Pessoa;
+DROP TABLE Telefone_pessoa;
+DROP TABLE Telefone_centro;
+DROP TABLE Paciente;
+DROP TABLE Atendente;
+DROP TABLE Profissional_de_saude;
+DROP TABLE Turno;
+DROP TABLE Area_atuacao;
+DROP TABLE Medico;
+DROP TABLE Medico_especialista;
+DROP TABLE Medico_geral;
+DROP TABLE Enfermeira;
+DROP TABLE Exame;
+DROP TABLE Laudo;
+DROP TABLE Exame_descricao;
+DROP TABLE Execucao;
+DROP TABLE Agendamento;
+DROP TABLE Agendamento_criado;
+DROP TABLE Acompanhante;
+
+-- Tabela Centro
+CREATE TABLE Centro(id_centro INTEGER, capacidade INTEGER, 
+	bairro VARCHAR2(20), complemento VARCHAR2(20), 
+	cidade VARCHAR2(20), rua VARCHAR2(40), numero INTEGER, 
+	estado VARCHAR2(2), cep VARCHAR2(9), pais VARCHAR2(20), 
+	CONSTRAINT centro_pkey PRIMARY KEY(id_centro))
+
+-- Tabela Pessoa
+CREATE TABLE Pessoa(cpf_pessoa VARCHAR2(11), nome VARCHAR2(40), 
+	sexo CHAR(1) CHECK (sexo in ('F', 'M')), data_de_nascimento DATE, 
+	bairro VARCHAR2(35), complemento VARCHAR2(20), cidade VARCHAR2(35), 
+	rua VARCHAR2(40), numero INTEGER, estado VARCHAR2(2), cep VARCHAR2(8), 
+	pais VARCHAR2(20), CONSTRAINT pessoa_pkey PRIMARY KEY(cpf_pessoa));
+
+-- Tabela Telefone_pessoa
+CREATE TABLE Telefone_pessoa(dono VARCHAR2(11), numero VARCHAR2(16),
+	CONSTRAINT telefone_pessoa_pkey PRIMARY KEY(dono, numero),
+	CONSTRAINT telefone_pessoa_fkey FOREIGN KEY(dono) REFERENCES Pessoa(cpf_pessoa));
+
+-- Tabela Telefone_centro
+CREATE TABLE Telefone_centro(id_centro INTEGER, numero VARCHAR2(16),
+	CONSTRAINT telefone_centro_pkey PRIMARY KEY(id_centro, numero),
+	CONSTRAINT telefone_centro_fkey FOREIGN KEY(id_centro) REFERENCES Centro(id_centro));
+
+-- Tabela Paciente
+CREATE TABLE Paciente(cpf_paciente VARCHAR2(11),
+	CONSTRAINT paciente_pkey PRIMARY KEY(cpf_paciente),
+	CONSTRAINT paciente_fkey FOREIGN KEY(cpf_paciente) REFERENCES Pessoa (cpf_pessoa));
+
+-- Tabela Acompanhante
+CREATE TABLE Acompanhante(cpf_paciente VARCHAR2(11), nome VARCHAR2(30), 
+	parentesco VARCHAR2(10), CONSTRAINT acompanhante_pkey PRIMARY KEY(cpf_paciente, nome),
+	CONSTRAINT acompanhante_fkey FOREIGN KEY(cpf_paciente) REFERENCES Paciente (cpf_paciente));
+
+-- Tabela Atendente
+CREATE TABLE Atendente(cpf_atendente VARCHAR2(11), 
+	centro INTEGER, CONSTRAINT atendente_pkey PRIMARY KEY(cpf_atendente),
+	CONSTRAINT atendente_fkey1 FOREIGN KEY(cpf_atendente) REFERENCES Pessoa(cpf_pessoa),
+	CONSTRAINT atendente_fkey2 FOREIGN KEY(centro) REFERENCES Centro(id_centro));
+
+-- Tabela Profissional_de_saude
+CREATE TABLE Profissional_de_saude(cpf_profissional VARCHAR2(11), 
+										cpf_gerente VARCHAR2(11), centro INTEGER, 
+	CONSTRAINT prof_saude_pkey PRIMARY KEY(cpf_profissional),
+	CONSTRAINT prof_saude_fkey1 FOREIGN KEY(cpf_profissional) REFERENCES Pessoa(cpf_pessoa),
+	CONSTRAINT prof_saude_fkey2 FOREIGN KEY(centro) REFERENCES Centro(id_centro),
+	CONSTRAINT prof_saude_fkey3 FOREIGN KEY(cpf_gerente) REFERENCES Profissional_de_saude (cpf_profissional));
+
+-- Tabela Turno
+CREATE TABLE Turno(profissional VARCHAR2(11), 
+				   periodo CHAR(1) CHECK (periodo in ('M', 'T', 'N')), 
+	CONSTRAINT turno_pkey PRIMARY KEY(profissional, periodo),
+	CONSTRAINT turno_fkey FOREIGN KEY(profissional) REFERENCES Profissional_de_saude (cpf_profissional));
+
+-- Tabela Area_de_atuacao
+CREATE TABLE Area_atuacao(profissional VARCHAR2(11), area VARCHAR2(40), 
+	CONSTRAINT area_pkey PRIMARY KEY(profissional, area),
+	CONSTRAINT area_fkey FOREIGN KEY(profissional) REFERENCES Profissional_de_saude (cpf_profissional));
+	
+-- Tabela Enfermeira
+CREATE TABLE Enfermeira(cpf_enfermeira VARCHAR2(11), COREN VARCHAR2(20) NOT NULL,
+	CONSTRAINT enfermeira_pkey PRIMARY KEY(cpf_enfermeira),
+	CONSTRAINT medico_unique UNIQUE (COREN),
+	CONSTRAINT enfermeira_fkey FOREIGN KEY(cpf_enfermeira) REFERENCES Profissional_de_saude(cpf_profissional));
+	
+-- Tabela Medico
+CREATE TABLE Medico(cpf_medico VARCHAR2(11), CRM VARCHAR2(20) NOT NULL,
+	CONSTRAINT medico_pkey PRIMARY KEY(cpf_medico),
+	CONSTRAINT medico_unique UNIQUE (CRM),
+	CONSTRAINT medico_fkey FOREIGN KEY(cpf_medico) REFERENCES Profissional_de_saude(cpf_profissional));
+
+-- Tabela Medico_especialista
+CREATE TABLE Medico_especialista(cpf_especialista VARCHAR2(11), 
+											   especialidade VARCHAR2(30),
+	CONSTRAINT medico_esp_pkey PRIMARY KEY(cpf_especialista),
+	CONSTRAINT medico_esp_fkey FOREIGN KEY(cpf_especialista) REFERENCES Medico(cpf_medico));
+
+-- Tabela Medico_geral
+CREATE TABLE Medico_geral(cpf_geral VARCHAR2(11),
+	CONSTRAINT medico_geral_pkey PRIMARY KEY(cpf_geral),
+	CONSTRAINT medico_geral_fkey FOREIGN KEY(cpf_geral) REFERENCES Medico(cpf_medico));
+
+-- Tabela Exame
+CREATE TABLE Exame(nome varchar2(30), id_centro integer, preco number(7,2),
+	CONSTRAINT exame_pkey PRIMARY KEY(nome),
+	CONSTRAINT exame_fkey FOREIGN KEY(id_centro) REFERENCES Centro (id_centro));
+
+-- Tabela Exame_descricao
+CREATE TABLE Exame_descricao(nome varchar2(30), descricao varchar2(255), 
+	CONSTRAINT exame_desc_pkey PRIMARY KEY(nome),
+	CONSTRAINT exame_desc_fkey FOREIGN KEY(nome) REFERENCES Exame(nome));
+
+-- Tabela Laudo
+CREATE TABLE Laudo(cpf_profissional VARCHAR2(11), cpf_paciente VARCHAR2(11),
+				   nome_exame VARCHAR2(30), resultado VARCHAR2(255),
+	CONSTRAINT laudo_pkey PRIMARY KEY(cpf_profissional, cpf_paciente),
+	CONSTRAINT laudo_fkey1 FOREIGN KEY(cpf_profissional) REFERENCES Profissional_de_saude(cpf_profissional),
+	CONSTRAINT laudo_fkey2 FOREIGN KEY(cpf_paciente) REFERENCES Paciente(cpf_paciente),
+	CONSTRAINT laudo_fkey3 FOREIGN KEY(nome_exame) REFERENCES Exame(nome));
+
+-- Tabela Execução
+CREATE TABLE Execucao(cpf_profissional varchar2(11), hora timestamp, 
+									id_exame varchar2(30),
+	CONSTRAINT exec_pkey PRIMARY KEY(cpf_profissional, hora, id_exame),
+	CONSTRAINT exec_fkey1 FOREIGN KEY(cpf_profissional) REFERENCES Profissional_de_saude(cpf_profissional),
+	CONSTRAINT exec_fkey2 FOREIGN KEY(id_exame) REFERENCES Exame(nome));
+
+-- Tabela Agendamento
+CREATE TABLE Agendamento(cpf_profissional varchar2(11), 
+				cpf_paciente varchar2(11), cpf_atendente varchar2(11), 
+				hora_marcacao timestamp, hora_agendamento timestamp, 
+				finalidade varchar2(20),
+	CONSTRAINT agend_pkey PRIMARY KEY(cpf_profissional, cpf_paciente),
+	CONSTRAINT agend_fkey1 FOREIGN KEY(cpf_profissional) REFERENCES Profissional_de_saude(cpf_profissional),
+	CONSTRAINT agend_fkey2 FOREIGN KEY(cpf_paciente) REFERENCES Paciente(cpf_paciente),
+	CONSTRAINT agend_fkey3 FOREIGN KEY(cpf_atendente) REFERENCES Atendente(cpf_atendente));
+
+-- Tabela Agendamentos_criados
+CREATE TABLE Agendamento_criado(cpf_atendente varchar2(11), 
+					 hora_marcacao timestamp, hora_agendamento timestamp,
+	CONSTRAINT agend_criado_pkey PRIMARY KEY(cpf_atendente),
+	CONSTRAINT agend_criado_fkey1 FOREIGN KEY(cpf_atendente) REFERENCES Pessoa(cpf_pessoa))
---
+++ inserts.sql
@@ -0,0 +1,277 @@
+INSERT INTO Pessoa VALUES ('74846020940', 'Agatha Tânia da Cunha', 'F', TO_DATE('23/03/1987', 'dd/mm/yyyy'), 'Alto Santa Maria', null, 'Parnaíba', 'Rua Doutor Zilmar Duarte Vieira', 746, 'PI', '64212480', 'Brasil');
+INSERT INTO Pessoa VALUES ('60220992851', 'Márcio Yuri André Melo', 'M', 
+TO_DATE('26/02/1958', 'dd/mm/yyyy'), 'Jardim Alto da Boa Vista', null, 'Umuarama', 
+'Rua Marcos Moreno dos Santos Cabeção', 778, 'PR', '87506460', 'Brasil');
+INSERT INTO Pessoa VALUES ('29874723580', 'Nina Marina Aline Moreira', 'F', 
+TO_DATE('05/01/2003', 'dd/mm/yyyy'), 'Ivete Vargas', null, 'Rio Branco', 
+'Rua 13 de Maio', 939, 'AC', '69900205', 'Brasil');
+INSERT INTO Pessoa VALUES ('65039251742', 'Kevin Luan Drumond', 'M', 
+TO_DATE('03/03/1984', 'dd/mm/yyyy'), 'Boa Esperança', null, 'Parnaíba', 
+'Rua Sete', 174, 'PI', '64215428', 'Brasil');
+INSERT INTO Pessoa VALUES ('07995976437', 'Diego Benício André Ribeiro', 'M', 
+TO_DATE('06/03/1964', 'dd/mm/yyyy'), 'Chácaras Araguaia', null, 'Novo Gama', 
+'Quadra Quadra 31', 941, 'GO', '72861124', 'Brasil');
+INSERT INTO Pessoa VALUES ('74075272451', 'Raimundo Isaac Mendes', 'M', 
+TO_DATE('20/01/1975', 'dd/mm/yyyy'), 'Jardim Saúde', null, 'Cacoal', 
+'Avenida das Comunicações', 922, 'RO', '76964173', 'Brasil');
+INSERT INTO Pessoa VALUES ('48487552986', 'Flávia Jéssica da Costa', 'F', 
+TO_DATE('01/02/1966', 'dd/mm/yyyy'), 'Colônia Oliveira Machado', null, 'Manaus', 
+'Rua Elesbão Luz', 244, 'AM', '69070800', 'Brasil');
+INSERT INTO Pessoa VALUES ('78822220030', 'Rafaela Sophie Raimunda Novaes', 'F', 
+TO_DATE('08/02/1981', 'dd/mm/yyyy'), 'Nossa Senhora das Graças', null, 'Caxias do Sul', 
+'Rua Lenice Moraes Santos', 545, 'RS', '95095143', 'Brasil');
+INSERT INTO Pessoa VALUES ('12826023497', 'Bárbara Stella da Mata', 'F', 
+TO_DATE('14/02/1957', 'dd/mm/yyyy'), 'Sotelândia', null, 'Cariacica', 
+'Rua E', 526, 'ES', '29140636', 'Brasil');
+INSERT INTO Pessoa VALUES ('69933068326', 'Elisa Camila Teixeira', 'F', 
+TO_DATE('08/01/1990', 'dd/mm/yyyy'), 'Pinheirinho', null, 'Francisco Beltrão', 
+'Rua Ângelo Pelisser', 566, 'PR', '85603173', 'Brasil');
+INSERT INTO Pessoa VALUES ('50413745384', 'Lavínia Juliana Sophia Dias', 'F', 
+TO_DATE('03/01/2002', 'dd/mm/yyyy'), 'Harmonia', null, 'Canoas', 
+'Rua das Araucárias', 105, 'RS', '92320810', 'Brasil');
+INSERT INTO Pessoa VALUES ('52236657404', 'Rosângela Francisca Isabelle', 'F', 
+TO_DATE('23/01/1975', 'dd/mm/yyyy'), 'Vila Amazonas', null, 'Santana', 
+'Rua D 6C', 925, 'AP', '68926210', 'Brasil');
+INSERT INTO Pessoa VALUES ('84705091957', 'Rodrigo José Thomas Rodrigues', 'M', 
+TO_DATE('24/02/1974', 'dd/mm/yyyy'), 'Sussuarana', null, 'Salvador', 
+'1ª Travessa Donana', 213, 'BA', '41214725', 'Brasil');
+INSERT INTO Pessoa VALUES ('26706295981', 'Osvaldo Lucas Kevin Brito', 'M', 
+TO_DATE('24/02/1974', 'dd/mm/yyyy'), 'Jardim Marília', null, 'Dourados', 
+'Rua Ceres', 468, 'MS', '79841604', 'Brasil');
+INSERT INTO Pessoa VALUES ('85851790660', 'Bento Carlos Eduardo Roberto Barros', 'M', 
+TO_DATE('27/02/1988', 'dd/mm/yyyy'), 'Parque do Sol', null, 'São José do Rio Preto', 
+'Rua José Cândido Rosa', 105, 'SP', '15040515', 'Brasil');
+INSERT INTO Pessoa VALUES ('49371521732', 'Fábio Enzo Benjamin da Rocha', 'M', 
+TO_DATE('14/02/1982', 'dd/mm/yyyy'), 'Parque dos Ipês II', null, 'Ponta Porã', 
+'Rua Hortêncio Vieira', 988, 'MS', '69900658', 'Brasil');
+INSERT INTO Pessoa VALUES ('18413380871', 'Mariah Débora Assis', 'F', 
+TO_DATE('14/03/1966', 'dd/mm/yyyy'), 'Bosque', null, 'Rio Branco', 
+'Rua Coronel Alexandrino', 351, 'AC', '76964173', 'Brasil');
+INSERT INTO Pessoa VALUES ('75834244025', 'Mirella Alícia Lima', 'F', 
+TO_DATE('12/03/1956', 'dd/mm/yyyy'), 'Água Verde', null, 'Blumenau', 
+'Rua Bubi Rocha Coutinho Júnior', 973, 'SC', '89042350', 'Brasil');
+INSERT INTO Pessoa VALUES ('77299804794', 'Kauê Ruan Assunção', 'M', 
+TO_DATE('22/01/1955', 'dd/mm/yyyy'), 'Aerolândia', null, 'Fortaleza', 
+'Rua do Partido', 744, 'CE', '60850300', 'Brasil');
+INSERT INTO Pessoa VALUES ('77897215504', 'Luís Ricardo Martins', 'M', 
+TO_DATE('27/02/1968', 'dd/mm/yyyy'), 'Bento Ferreira', null, 'Vitória', 
+'Rua João Balbi', 813, 'ES', '29050725', 'Brasil');
+INSERT INTO Pessoa VALUES ('54201541029', 'Raimunda Flávia Clarice Silveira', 'F', 
+TO_DATE('04/01/2001', 'dd/mm/yyyy'), 'Plano Diretor Sul', null, 'Palmas', 
+'Quadra 207 Sul Alameda 11', 221, 'TO', '77015292', 'Brasil');
+INSERT INTO Pessoa VALUES ('23369225255', 'Valentina Manuela Lopes', 'F', 
+TO_DATE('03/02/1944', 'dd/mm/yyyy'), 'Universidade', null, 'Macapá', 
+'Rua Zéca Serra', 318, 'AP', '68903499', 'Brasil');
+INSERT INTO Pessoa VALUES ('29705630704', 'Alice Amanda Fogaça', 'F', 
+TO_DATE('24/02/1956', 'dd/mm/yyyy'), 'Bela Vista', null, 'Fortaleza', 
+'Rua 1', 256, 'CE', '60442115', 'Brasil');
+INSERT INTO Pessoa VALUES ('48466958401', 'Manoel Benício Theo Cardoso', 'M', 
+TO_DATE('11/01/1969', 'dd/mm/yyyy'), 'Santo Antônio', null, 'Teresina', 
+'Quadra 07', 948, 'PI', '64033524', 'Brasil');
+INSERT INTO Pessoa VALUES ('72376837219', 'Jéssica Jaqueline Gomes', 'F', 
+TO_DATE('15/01/1988', 'dd/mm/yyyy'), 'Velame', null, 'Campina Grande', 
+'Rua Santino Freire Andrade', 483, 'PB', '58420133', 'Brasil');
+INSERT INTO Pessoa VALUES ('19125818414', 'Camila Sueli Moraes', 'F', 
+TO_DATE('30/03/1956', 'dd/mm/yyyy'), 'Copas Verdes', null, 'Ji-Paraná', 
+'Rua João Pedro Batista', 952, 'RO', '76901484', 'Brasil');
+INSERT INTO Pessoa VALUES ('29021059088', 'Maya Daiane Vieira', 'F', 
+TO_DATE('31/03/1953', 'dd/mm/yyyy'), 'Realengo', null, 'Rio de Janeiro', 
+'Rua Quatro', 588, 'RJ', '21770390', 'Brasil');
+INSERT INTO Pessoa VALUES ('74162082030', 'Nair Carolina Valentina Alves', 'F', 
+TO_DATE('18/02/1989', 'dd/mm/yyyy'), 'Nova Santa Cruz', null, 'Santa Cruz do Capibaribe', 
+'Rua João Chagas Sobrinho', 588, 'PE', '55194348', 'Brasil');
+INSERT INTO Pessoa VALUES ('25483157036', 'Nelson Manoel Pires', 'M', 
+TO_DATE('15/03/1962', 'dd/mm/yyyy'), 'Parque Industrial', null, 'Campinas', 
+'Rua Professor Orestes Carlos Segallio', 866, 'SP', '13031540', 'Brasil');
+INSERT INTO Pessoa VALUES ('89974154685', 'Carlos Benedito Peixoto', 'M', 
+TO_DATE('12/03/1981', 'dd/mm/yyyy'), 'Bom Jesus', null, 'Porto Alegre', 
+'Rua B', 995, 'RS', '91420092', 'Brasil');
+
+
+CREATE SEQUENCE centro_seq
+  MINVALUE 1
+  START WITH 1
+  INCREMENT BY 1
+  NOCACHE;
+
+
+INSERT INTO Centro VALUES (centro_seq.NEXTVAL, 100, 'Rendeiras', null, 'Barnaúba', 'Rua Marechal Jorge Gomes', 192, 'SP', 5995782, 'Brasil');
+INSERT INTO Centro VALUES (centro_seq.NEXTVAL, 80, 'São José', null, 'Parnaíba', 'Rua Santa Catarina', 2813, 'PI', 69334233, 'Brasil');
+INSERT INTO Centro VALUES (centro_seq.NEXTVAL, 70, 'Centro', null, 'Ariquemes', 'Travessa Antônio Ferreira', 213, 'PA', 68700216, 'Brasil');
+INSERT INTO Centro VALUES (centro_seq.NEXTVAL, 60, 'Vila Gomes Cardim', null, 'São Paulo', 'Rua Serra de Bragança', 172, 'SP', 03318000, 'Brasil');
+INSERT INTO Centro VALUES (centro_seq.NEXTVAL, 120, 'República', null, 'Jundiaí', 'Avenida São João', 264, 'SP', 13216000, 'Brasil');
+INSERT INTO Centro VALUES (centro_seq.NEXTVAL, 75, 'Nazaré', null, 'Belém', 'Avenida Governador José Malcher', 63, 'PR', 01045001, 'Brasil');
+
+INSERT INTO Telefone_centro VALUES (1, '(11) 3760-3346');
+INSERT INTO Telefone_centro VALUES (1, '(11) 2665-4205');
+INSERT INTO Telefone_centro VALUES (2, '(86) 2134-3872');
+INSERT INTO Telefone_centro VALUES (2, '(86) 2619-1733');
+INSERT INTO Telefone_centro VALUES (3, '(91) 3753-3422');
+INSERT INTO Telefone_centro VALUES (3, '(91) 3457-1264');
+INSERT INTO Telefone_centro VALUES (4, '(11) 3579-5366');
+INSERT INTO Telefone_centro VALUES (4, '(11) 2324-4950');
+INSERT INTO Telefone_centro VALUES (5, '(11) 2842-6248');
+INSERT INTO Telefone_centro VALUES (5, '(11) 3120-5513');
+INSERT INTO Telefone_centro VALUES (6, '(41) 2421-5433');
+INSERT INTO Telefone_centro VALUES (6, '(41) 2516-1655');
+INSERT INTO Telefone_pessoa VALUES ('07995976437', '(63) 92252-4716');
+INSERT INTO Telefone_pessoa VALUES ('07995976437', '(49) 3121-2123');
+INSERT INTO Telefone_pessoa VALUES ('12826023497', '(97) 92785-9663');
+INSERT INTO Telefone_pessoa VALUES ('18413380871', '(86) 92543-3273');
+INSERT INTO Telefone_pessoa VALUES ('18413380871', '(22) 92788-7376');
+INSERT INTO Telefone_pessoa VALUES ('19125818414', '(68) 93515-4253');
+INSERT INTO Telefone_pessoa VALUES ('23369225255', '(32) 92618-2466');
+INSERT INTO Telefone_pessoa VALUES ('25483157036', '(48) 92828-2692');
+INSERT INTO Telefone_pessoa VALUES ('29021059088', '(93) 92220-2285');
+INSERT INTO Telefone_pessoa VALUES ('29021059088', '(27) 92451-3687');
+INSERT INTO Telefone_pessoa VALUES ('29021059088', '(69) 92760-6098');
+INSERT INTO Telefone_pessoa VALUES ('29705630704', '(64) 93985-3176');
+INSERT INTO Telefone_pessoa VALUES ('29874723580', '(87) 93550-1352');
+INSERT INTO Telefone_pessoa VALUES ('48466958401', '(49) 93475-8490');
+INSERT INTO Telefone_pessoa VALUES ('48487552986', '(97) 93696-5923');
+INSERT INTO Telefone_pessoa VALUES ('49371521732', '(84) 93870-9773');
+INSERT INTO Telefone_pessoa VALUES('50413745384', '(16) 92167-4413');
+INSERT INTO Telefone_pessoa VALUES ('54201541029', '(18) 93351-5586');
+INSERT INTO Telefone_pessoa VALUES ('60220992851', '(88) 92106-0527');
+INSERT INTO Telefone_pessoa VALUES ('65039251742', '(98) 93456-1129');
+INSERT INTO Telefone_pessoa VALUES ('69933068326', '(83) 93630-5606');
+INSERT INTO Telefone_pessoa VALUES ('72376837219', '(83) 92174-1557');
+INSERT INTO Telefone_pessoa VALUES ('74075272451', '(96) 93804-8071');
+INSERT INTO Telefone_pessoa VALUES ('74162082030', '(89) 92887-6178');
+INSERT INTO Telefone_pessoa VALUES ('74846020940', '(79) 92437-3660');
+INSERT INTO Telefone_pessoa VALUES ('75834244025', '(38) 92814-5214');
+INSERT INTO Telefone_pessoa VALUES ('77299804794', '(94) 92753-7231');
+INSERT INTO Telefone_pessoa VALUES ('77897215504', '(43) 93453-1887');
+INSERT INTO Telefone_pessoa VALUES ('78822220030', '(48) 93526-3377');
+INSERT INTO Telefone_pessoa VALUES ('84705091957', '(27) 93645-2206');
+INSERT INTO Telefone_pessoa VALUES ('85851790660', '(53) 93642-7673');
+INSERT INTO Telefone_pessoa VALUES ('89974154685', '(38) 93046-8488');
+
+INSERT INTO Paciente VALUES ('07995976437');
+INSERT INTO Paciente VALUES ('12826023497');
+INSERT INTO Paciente VALUES ('18413380871');
+INSERT INTO Paciente VALUES ('19125818414');
+INSERT INTO Paciente VALUES ('23369225255');
+INSERT INTO Paciente VALUES ('25483157036');
+INSERT INTO Paciente VALUES ('29021059088');
+
+INSERT INTO Atendente VALUES ('29705630704', 1);
+INSERT INTO Atendente VALUES ('29874723580', 2);
+INSERT INTO Atendente VALUES ('48466958401', 3);
+INSERT INTO Atendente VALUES ('48487552986', 4);
+INSERT INTO Atendente VALUES ('49371521732', 5);
+INSERT INTO Atendente VALUES ('50413745384', 6);
+
+INSERT INTO Acompanhante VALUES ('07995976437', 'Iseu Paulino Figueiredo', 'Filho');
+INSERT INTO Acompanhante VALUES ('07995976437', 'Erika Cruz Diovani', 'Tia');
+INSERT INTO Acompanhante VALUES ('12826023497', 'Alfredo Aguiar Jesus', 'Tio');
+INSERT INTO Acompanhante VALUES ('18413380871', 'Adilson Dores Castelete', 'Pai');
+INSERT INTO Acompanhante VALUES ('19125818414', 'Aderbal Vasconcelos Prata', 'Sobinho');
+INSERT INTO Acompanhante VALUES ('23369225255', 'Mirian Meyer Cretella', 'Prima');
+INSERT INTO Acompanhante VALUES ('25483157036', 'Thayná da Souza Barher', 'Mãe');
+INSERT INTO Acompanhante VALUES ('29021059088', 'Marcus Moraes Chiles', 'Filho');
+
+
+INSERT INTO Profissional_de_saude VALUES ('60220992851', null, 1);
+INSERT INTO Profissional_de_saude VALUES ('65039251742', '60220992851', 1);
+INSERT INTO Profissional_de_saude VALUES ('69933068326', null, 2);
+INSERT INTO Profissional_de_saude VALUES ('72376837219', '69933068326', 2);
+INSERT INTO Profissional_de_saude VALUES ('74162082030', null, 1);
+INSERT INTO Profissional_de_saude VALUES ('75834244025', null, 2);
+INSERT INTO Profissional_de_saude VALUES ('77299804794', '75834244025', 2);
+INSERT INTO Profissional_de_saude VALUES ('77897215504', null, 1);
+INSERT INTO Profissional_de_saude VALUES ('78822220030', '77897215504', 1);
+INSERT INTO Profissional_de_saude VALUES ('84705091957', null, 2);
+INSERT INTO Profissional_de_saude VALUES ('85851790660', '84705091957', 2);
+INSERT INTO Profissional_de_saude VALUES ('89974154685', '84705091957', 2);
+
+
+INSERT INTO Turno VALUES ('60220992851', 'M');
+INSERT INTO Turno VALUES ('60220992851', 'T');
+INSERT INTO Turno VALUES ('65039251742', 'T');
+INSERT INTO Turno VALUES ('69933068326', 'M');
+INSERT INTO Turno VALUES ('69933068326', 'T');
+INSERT INTO Turno VALUES ('72376837219', 'M');
+INSERT INTO Turno VALUES ('72376837219', 'T');
+INSERT INTO Turno VALUES ('74162082030', 'M');
+INSERT INTO Turno VALUES ('75834244025', 'T');
+INSERT INTO Turno VALUES ('77299804794', 'T');
+INSERT INTO Turno VALUES ('77897215504', 'T');
+INSERT INTO Turno VALUES ('78822220030', 'M');
+INSERT INTO Turno VALUES ('84705091957', 'T');
+INSERT INTO Turno VALUES ('85851790660', 'M');
+INSERT INTO Turno VALUES ('89974154685', 'M');
+
+INSERT INTO Area_atuacao VALUES ('60220992851', 'Area I da enfermagem');
+INSERT INTO Area_atuacao VALUES ('60220992851', 'Area II da enfermagem');
+INSERT INTO Area_atuacao VALUES ('65039251742', 'Area I da enfermagem');
+INSERT INTO Area_atuacao VALUES ('69933068326', 'Area I da enfermagem');
+INSERT INTO Area_atuacao VALUES ('72376837219', 'Area I da enfermagem');
+INSERT INTO Area_atuacao VALUES ('74162082030', 'Angiologia e cirurgia vascular:');
+INSERT INTO Area_atuacao VALUES ('75834244025', 'Cirurgia cardiovascular');
+INSERT INTO Area_atuacao VALUES ('77299804794', 'Ortopedia e traumatologia');
+INSERT INTO Area_atuacao VALUES ('77897215504', 'Clínica Médica');
+INSERT INTO Area_atuacao VALUES ('78822220030', 'Neurologia');
+INSERT INTO Area_atuacao VALUES ('78822220030', 'Clínica Médica');
+INSERT INTO Area_atuacao VALUES ('84705091957', 'Clínica Médica');
+INSERT INTO Area_atuacao VALUES ('85851790660', 'Clínica Médica');
+INSERT INTO Area_atuacao VALUES ('89974154685', 'Clínica Médica');
+
+
+INSERT INTO Enfermeira VALUES ('60220992851', '464256');
+INSERT INTO Enfermeira VALUES ('65039251742', '586481');
+INSERT INTO Enfermeira VALUES ('69933068326', '326585');
+INSERT INTO Enfermeira VALUES ('72376837219', '918270');
+
+
+INSERT INTO Medico VALUES ('74162082030', '110649');
+INSERT INTO Medico VALUES ('75834244025', '93331');
+INSERT INTO Medico VALUES ('77299804794', '46291');
+INSERT INTO Medico VALUES ('77897215504', '697319');
+INSERT INTO Medico VALUES ('78822220030', '267976');
+INSERT INTO Medico VALUES ('84705091957', '708010');
+INSERT INTO Medico VALUES ('85851790660', '488382');
+INSERT INTO Medico VALUES ('89974154685', '812270');
+
+
+INSERT INTO Medico_geral VALUES ('77897215504');
+INSERT INTO Medico_geral VALUES ('78822220030');
+INSERT INTO Medico_geral VALUES ('84705091957');
+INSERT INTO Medico_geral VALUES ('85851790660');
+INSERT INTO Medico_geral VALUES ('89974154685');
+
+INSERT INTO Medico_especialista VALUES ('74162082030', 'Angiologia');
+INSERT INTO Medico_especialista VALUES ('74846020940', 'Urologia');
+INSERT INTO Medico_especialista VALUES ('77299804794', 'Ortopedia');
+
+INSERT INTO Exame VALUES ('Hemograma', 1, 50.00);
+INSERT INTO Exame VALUES ('Papanicolau', 1, 100.00);
+INSERT INTO Exame VALUES ('TGP e TGO', 1, 100.00);
+INSERT INTO Exame VALUES ('TSH', 1, 18.00);
+INSERT INTO Exame VALUES ('Ultrassonografia', 2, 100.00);
+
+
+INSERT INTO Exame_descricao VALUES('Hemograma','Tipo de exame que analisa informações específicas sobre os tipos e quantidades dos componentes no sangue.' );
+INSERT INTO Exame_descricao VALUES('Papanicolau', 'Ajuda a prevenir o câncer no colo de útero e também doenças como o HPV, infecções vaginais e doenças sexualmente transmissíveis');
+INSERT INTO Exame_descricao VALUES('TGP e TGO', 'Exames laboratoriais que se referem à saúde do fígado');
+INSERT INTO Exame_descricao VALUES('TSH', 'Exames laboratoriais para analisar o funcionamento da tireoide.');
+INSERT INTO Exame_descricao VALUES('Ultrassonografia', 'A ultrassonografia serve para avaliar as estruturas corporais em tempo real. ');
+
+INSERT INTO Laudo VALUES('60220992851','18413380871', 'Hemograma', 'lorem ipsum');
+
+
+INSERT INTO Execucao VALUES('60220992851', TO_TIMESTAMP('26/04/2022 10:13:18', 'DD/MM/YYYY HH:MI:SS'), 'Hemograma');
+
+INSERT INTO Agendamento VALUES('84705091957', '29021059088', '48487552986',
+TO_TIMESTAMP('14/03/2022 10:13:18', 'DD/MM/YYYY HH:MI:SS'), 
+TO_TIMESTAMP('21/05/2022 10:13:18', 'DD/MM/YYYY HH:MI:SS'), 'Finalidade');
+INSERT INTO Agendamento VALUES('85851790660', '19125818414', '50413745384',
+TO_TIMESTAMP('31/03/2022 10:13:18', 'DD/MM/YYYY HH:MI:SS'), 
+TO_TIMESTAMP('22/04/2022 10:13:18', 'DD/MM/YYYY HH:MI:SS'), 'Finalidade');
+
+INSERT INTO Agendamento_criado VALUES('48487552986',
+TO_TIMESTAMP('14/05/2022 10:13:18', 'DD/MM/YYYY HH:MI:SS'), 
+TO_TIMESTAMP('14/06/2022 10:13:18', 'DD/MM/YYYY HH:MI:SS'));
+INSERT INTO Agendamento_criado VALUES('50413745384',
+TO_TIMESTAMP('31/03/2022 10:13:18', 'DD/MM/YYYY HH:MI:SS'), 
+TO_TIMESTAMP('22/04/2022 10:13:18', 'DD/MM/YYYY HH:MI:SS'));
