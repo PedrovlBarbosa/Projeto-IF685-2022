f17e78a5c435b0a8fb813695bdbe98692c78fcef
Danilo Silva <49529180+JDaniloC@users.noreply.github.com>
2022-04-05 10:03:29 -0300

    Add truncate

--- inserts.sql
+++ inserts.sql
@@ -1,91 +1,99 @@
-INSERT INTO Pessoa VALUES ('74846020940', 'Agatha Tânia da Cunha', 'F', TO_DATE('23/03/1987', 'dd/mm/yyyy'), 'Alto Santa Maria', null, 'Parnaíba', 'Rua Doutor Zilmar Duarte Vieira', 746, 'PI', '64212480', 'Brasil');
+TRUNCATE TABLE Telefone_pessoa;
+TRUNCATE TABLE Pessoa;
+
+TRUNCATE TABLE Telefone_centro;
+TRUNCATE TABLE Centro;
+
+INSERT INTO Pessoa VALUES ('74846020940', 'Agatha Tânia da Cunha', 'F', 
+    TO_DATE('23/03/1987', 'dd/mm/yyyy'), 'Alto Santa Maria', null, 
+    'Parnaíba', 'Rua Doutor Zilmar Duarte Vieira', 746, 'PI', '64212480', 'Brasil');
 INSERT INTO Pessoa VALUES ('60220992851', 'Márcio Yuri André Melo', 'M', 
-TO_DATE('26/02/1958', 'dd/mm/yyyy'), 'Jardim Alto da Boa Vista', null, 'Umuarama', 
-'Rua Marcos Moreno dos Santos Cabeção', 778, 'PR', '87506460', 'Brasil');
+    TO_DATE('26/02/1958', 'dd/mm/yyyy'), 'Jardim Alto da Boa Vista', null, 'Umuarama', 
+    'Rua Marcos Moreno dos Santos Cabeção', 778, 'PR', '87506460', 'Brasil');
 INSERT INTO Pessoa VALUES ('29874723580', 'Nina Marina Aline Moreira', 'F', 
-TO_DATE('05/01/2003', 'dd/mm/yyyy'), 'Ivete Vargas', null, 'Rio Branco', 
-'Rua 13 de Maio', 939, 'AC', '69900205', 'Brasil');
+    TO_DATE('05/01/2003', 'dd/mm/yyyy'), 'Ivete Vargas', null, 'Rio Branco', 
+    'Rua 13 de Maio', 939, 'AC', '69900205', 'Brasil');
 INSERT INTO Pessoa VALUES ('65039251742', 'Kevin Luan Drumond', 'M', 
-TO_DATE('03/03/1984', 'dd/mm/yyyy'), 'Boa Esperança', null, 'Parnaíba', 
-'Rua Sete', 174, 'PI', '64215428', 'Brasil');
+    TO_DATE('03/03/1984', 'dd/mm/yyyy'), 'Boa Esperança', null, 'Parnaíba', 
+    'Rua Sete', 174, 'PI', '64215428', 'Brasil');
 INSERT INTO Pessoa VALUES ('07995976437', 'Diego Benício André Ribeiro', 'M', 
-TO_DATE('06/03/1964', 'dd/mm/yyyy'), 'Chácaras Araguaia', null, 'Novo Gama', 
-'Quadra Quadra 31', 941, 'GO', '72861124', 'Brasil');
+    TO_DATE('06/03/1964', 'dd/mm/yyyy'), 'Chácaras Araguaia', null, 'Novo Gama', 
+    'Quadra Quadra 31', 941, 'GO', '72861124', 'Brasil');
 INSERT INTO Pessoa VALUES ('74075272451', 'Raimundo Isaac Mendes', 'M', 
-TO_DATE('20/01/1975', 'dd/mm/yyyy'), 'Jardim Saúde', null, 'Cacoal', 
-'Avenida das Comunicações', 922, 'RO', '76964173', 'Brasil');
+    TO_DATE('20/01/1975', 'dd/mm/yyyy'), 'Jardim Saúde', null, 'Cacoal', 
+    'Avenida das Comunicações', 922, 'RO', '76964173', 'Brasil');
 INSERT INTO Pessoa VALUES ('48487552986', 'Flávia Jéssica da Costa', 'F', 
-TO_DATE('01/02/1966', 'dd/mm/yyyy'), 'Colônia Oliveira Machado', null, 'Manaus', 
-'Rua Elesbão Luz', 244, 'AM', '69070800', 'Brasil');
+    TO_DATE('01/02/1966', 'dd/mm/yyyy'), 'Colônia Oliveira Machado', null, 'Manaus', 
+    'Rua Elesbão Luz', 244, 'AM', '69070800', 'Brasil');
 INSERT INTO Pessoa VALUES ('78822220030', 'Rafaela Sophie Raimunda Novaes', 'F', 
-TO_DATE('08/02/1981', 'dd/mm/yyyy'), 'Nossa Senhora das Graças', null, 'Caxias do Sul', 
-'Rua Lenice Moraes Santos', 545, 'RS', '95095143', 'Brasil');
+    TO_DATE('08/02/1981', 'dd/mm/yyyy'), 'Nossa Senhora das Graças', null, 'Caxias do Sul', 
+    'Rua Lenice Moraes Santos', 545, 'RS', '95095143', 'Brasil');
 INSERT INTO Pessoa VALUES ('12826023497', 'Bárbara Stella da Mata', 'F', 
-TO_DATE('14/02/1957', 'dd/mm/yyyy'), 'Sotelândia', null, 'Cariacica', 
-'Rua E', 526, 'ES', '29140636', 'Brasil');
+    TO_DATE('14/02/1957', 'dd/mm/yyyy'), 'Sotelândia', null, 'Cariacica', 
+    'Rua E', 526, 'ES', '29140636', 'Brasil');
 INSERT INTO Pessoa VALUES ('69933068326', 'Elisa Camila Teixeira', 'F', 
-TO_DATE('08/01/1990', 'dd/mm/yyyy'), 'Pinheirinho', null, 'Francisco Beltrão', 
-'Rua Ângelo Pelisser', 566, 'PR', '85603173', 'Brasil');
+    TO_DATE('08/01/1990', 'dd/mm/yyyy'), 'Pinheirinho', null, 'Francisco Beltrão', 
+    'Rua Ângelo Pelisser', 566, 'PR', '85603173', 'Brasil');
 INSERT INTO Pessoa VALUES ('50413745384', 'Lavínia Juliana Sophia Dias', 'F', 
-TO_DATE('03/01/2002', 'dd/mm/yyyy'), 'Harmonia', null, 'Canoas', 
-'Rua das Araucárias', 105, 'RS', '92320810', 'Brasil');
+    TO_DATE('03/01/2002', 'dd/mm/yyyy'), 'Harmonia', null, 'Canoas', 
+    'Rua das Araucárias', 105, 'RS', '92320810', 'Brasil');
 INSERT INTO Pessoa VALUES ('52236657404', 'Rosângela Francisca Isabelle', 'F', 
-TO_DATE('23/01/1975', 'dd/mm/yyyy'), 'Vila Amazonas', null, 'Santana', 
-'Rua D 6C', 925, 'AP', '68926210', 'Brasil');
+    TO_DATE('23/01/1975', 'dd/mm/yyyy'), 'Vila Amazonas', null, 'Santana', 
+    'Rua D 6C', 925, 'AP', '68926210', 'Brasil');
 INSERT INTO Pessoa VALUES ('84705091957', 'Rodrigo José Thomas Rodrigues', 'M', 
-TO_DATE('24/02/1974', 'dd/mm/yyyy'), 'Sussuarana', null, 'Salvador', 
-'1ª Travessa Donana', 213, 'BA', '41214725', 'Brasil');
+    TO_DATE('24/02/1974', 'dd/mm/yyyy'), 'Sussuarana', null, 'Salvador', 
+    '1ª Travessa Donana', 213, 'BA', '41214725', 'Brasil');
 INSERT INTO Pessoa VALUES ('26706295981', 'Osvaldo Lucas Kevin Brito', 'M', 
-TO_DATE('24/02/1974', 'dd/mm/yyyy'), 'Jardim Marília', null, 'Dourados', 
-'Rua Ceres', 468, 'MS', '79841604', 'Brasil');
+    TO_DATE('24/02/1974', 'dd/mm/yyyy'), 'Jardim Marília', null, 'Dourados', 
+    'Rua Ceres', 468, 'MS', '79841604', 'Brasil');
 INSERT INTO Pessoa VALUES ('85851790660', 'Bento Carlos Eduardo Roberto Barros', 'M', 
-TO_DATE('27/02/1988', 'dd/mm/yyyy'), 'Parque do Sol', null, 'São José do Rio Preto', 
-'Rua José Cândido Rosa', 105, 'SP', '15040515', 'Brasil');
+    TO_DATE('27/02/1988', 'dd/mm/yyyy'), 'Parque do Sol', null, 'São José do Rio Preto', 
+    'Rua José Cândido Rosa', 105, 'SP', '15040515', 'Brasil');
 INSERT INTO Pessoa VALUES ('49371521732', 'Fábio Enzo Benjamin da Rocha', 'M', 
-TO_DATE('14/02/1982', 'dd/mm/yyyy'), 'Parque dos Ipês II', null, 'Ponta Porã', 
-'Rua Hortêncio Vieira', 988, 'MS', '69900658', 'Brasil');
+    TO_DATE('14/02/1982', 'dd/mm/yyyy'), 'Parque dos Ipês II', null, 'Ponta Porã', 
+    'Rua Hortêncio Vieira', 988, 'MS', '69900658', 'Brasil');
 INSERT INTO Pessoa VALUES ('18413380871', 'Mariah Débora Assis', 'F', 
-TO_DATE('14/03/1966', 'dd/mm/yyyy'), 'Bosque', null, 'Rio Branco', 
-'Rua Coronel Alexandrino', 351, 'AC', '76964173', 'Brasil');
+    TO_DATE('14/03/1966', 'dd/mm/yyyy'), 'Bosque', null, 'Rio Branco', 
+    'Rua Coronel Alexandrino', 351, 'AC', '76964173', 'Brasil');
 INSERT INTO Pessoa VALUES ('75834244025', 'Mirella Alícia Lima', 'F', 
-TO_DATE('12/03/1956', 'dd/mm/yyyy'), 'Água Verde', null, 'Blumenau', 
-'Rua Bubi Rocha Coutinho Júnior', 973, 'SC', '89042350', 'Brasil');
+    TO_DATE('12/03/1956', 'dd/mm/yyyy'), 'Água Verde', null, 'Blumenau', 
+    'Rua Bubi Rocha Coutinho Júnior', 973, 'SC', '89042350', 'Brasil');
 INSERT INTO Pessoa VALUES ('77299804794', 'Kauê Ruan Assunção', 'M', 
-TO_DATE('22/01/1955', 'dd/mm/yyyy'), 'Aerolândia', null, 'Fortaleza', 
-'Rua do Partido', 744, 'CE', '60850300', 'Brasil');
+    TO_DATE('22/01/1955', 'dd/mm/yyyy'), 'Aerolândia', null, 'Fortaleza', 
+    'Rua do Partido', 744, 'CE', '60850300', 'Brasil');
 INSERT INTO Pessoa VALUES ('77897215504', 'Luís Ricardo Martins', 'M', 
-TO_DATE('27/02/1968', 'dd/mm/yyyy'), 'Bento Ferreira', null, 'Vitória', 
-'Rua João Balbi', 813, 'ES', '29050725', 'Brasil');
+    TO_DATE('27/02/1968', 'dd/mm/yyyy'), 'Bento Ferreira', null, 'Vitória', 
+    'Rua João Balbi', 813, 'ES', '29050725', 'Brasil');
 INSERT INTO Pessoa VALUES ('54201541029', 'Raimunda Flávia Clarice Silveira', 'F', 
-TO_DATE('04/01/2001', 'dd/mm/yyyy'), 'Plano Diretor Sul', null, 'Palmas', 
-'Quadra 207 Sul Alameda 11', 221, 'TO', '77015292', 'Brasil');
+    TO_DATE('04/01/2001', 'dd/mm/yyyy'), 'Plano Diretor Sul', null, 'Palmas', 
+    'Quadra 207 Sul Alameda 11', 221, 'TO', '77015292', 'Brasil');
 INSERT INTO Pessoa VALUES ('23369225255', 'Valentina Manuela Lopes', 'F', 
-TO_DATE('03/02/1944', 'dd/mm/yyyy'), 'Universidade', null, 'Macapá', 
-'Rua Zéca Serra', 318, 'AP', '68903499', 'Brasil');
+    TO_DATE('03/02/1944', 'dd/mm/yyyy'), 'Universidade', null, 'Macapá', 
+    'Rua Zéca Serra', 318, 'AP', '68903499', 'Brasil');
 INSERT INTO Pessoa VALUES ('29705630704', 'Alice Amanda Fogaça', 'F', 
-TO_DATE('24/02/1956', 'dd/mm/yyyy'), 'Bela Vista', null, 'Fortaleza', 
-'Rua 1', 256, 'CE', '60442115', 'Brasil');
+    TO_DATE('24/02/1956', 'dd/mm/yyyy'), 'Bela Vista', null, 'Fortaleza', 
+    'Rua 1', 256, 'CE', '60442115', 'Brasil');
 INSERT INTO Pessoa VALUES ('48466958401', 'Manoel Benício Theo Cardoso', 'M', 
-TO_DATE('11/01/1969', 'dd/mm/yyyy'), 'Santo Antônio', null, 'Teresina', 
-'Quadra 07', 948, 'PI', '64033524', 'Brasil');
+    TO_DATE('11/01/1969', 'dd/mm/yyyy'), 'Santo Antônio', null, 'Teresina', 
+    'Quadra 07', 948, 'PI', '64033524', 'Brasil');
 INSERT INTO Pessoa VALUES ('72376837219', 'Jéssica Jaqueline Gomes', 'F', 
-TO_DATE('15/01/1988', 'dd/mm/yyyy'), 'Velame', null, 'Campina Grande', 
-'Rua Santino Freire Andrade', 483, 'PB', '58420133', 'Brasil');
+    TO_DATE('15/01/1988', 'dd/mm/yyyy'), 'Velame', null, 'Campina Grande', 
+    'Rua Santino Freire Andrade', 483, 'PB', '58420133', 'Brasil');
 INSERT INTO Pessoa VALUES ('19125818414', 'Camila Sueli Moraes', 'F', 
-TO_DATE('30/03/1956', 'dd/mm/yyyy'), 'Copas Verdes', null, 'Ji-Paraná', 
-'Rua João Pedro Batista', 952, 'RO', '76901484', 'Brasil');
+    TO_DATE('30/03/1956', 'dd/mm/yyyy'), 'Copas Verdes', null, 'Ji-Paraná', 
+    'Rua João Pedro Batista', 952, 'RO', '76901484', 'Brasil');
 INSERT INTO Pessoa VALUES ('29021059088', 'Maya Daiane Vieira', 'F', 
-TO_DATE('31/03/1953', 'dd/mm/yyyy'), 'Realengo', null, 'Rio de Janeiro', 
-'Rua Quatro', 588, 'RJ', '21770390', 'Brasil');
+    TO_DATE('31/03/1953', 'dd/mm/yyyy'), 'Realengo', null, 'Rio de Janeiro', 
+    'Rua Quatro', 588, 'RJ', '21770390', 'Brasil');
 INSERT INTO Pessoa VALUES ('74162082030', 'Nair Carolina Valentina Alves', 'F', 
-TO_DATE('18/02/1989', 'dd/mm/yyyy'), 'Nova Santa Cruz', null, 'Santa Cruz do Capibaribe', 
-'Rua João Chagas Sobrinho', 588, 'PE', '55194348', 'Brasil');
+    TO_DATE('18/02/1989', 'dd/mm/yyyy'), 'Nova Santa Cruz', null, 'Santa Cruz do Capibaribe', 
+    'Rua João Chagas Sobrinho', 588, 'PE', '55194348', 'Brasil');
 INSERT INTO Pessoa VALUES ('25483157036', 'Nelson Manoel Pires', 'M', 
-TO_DATE('15/03/1962', 'dd/mm/yyyy'), 'Parque Industrial', null, 'Campinas', 
-'Rua Professor Orestes Carlos Segallio', 866, 'SP', '13031540', 'Brasil');
+    TO_DATE('15/03/1962', 'dd/mm/yyyy'), 'Parque Industrial', null, 'Campinas', 
+    'Rua Professor Orestes Carlos Segallio', 866, 'SP', '13031540', 'Brasil');
 INSERT INTO Pessoa VALUES ('89974154685', 'Carlos Benedito Peixoto', 'M', 
-TO_DATE('12/03/1981', 'dd/mm/yyyy'), 'Bom Jesus', null, 'Porto Alegre', 
-'Rua B', 995, 'RS', '91420092', 'Brasil');
+    TO_DATE('12/03/1981', 'dd/mm/yyyy'), 'Bom Jesus', null, 'Porto Alegre', 
+    'Rua B', 995, 'RS', '91420092', 'Brasil');
 
 
 CREATE SEQUENCE centro_seq
@@ -93,7 +101,6 @@ CREATE SEQUENCE centro_seq
   START WITH 1
   INCREMENT BY 1
   NOCACHE;
-
 
 INSERT INTO Centro VALUES (centro_seq.NEXTVAL, 100, 'Rendeiras', null, 'Barnaúba', 'Rua Marechal Jorge Gomes', 192, 'SP', 5995782, 'Brasil');
 INSERT INTO Centro VALUES (centro_seq.NEXTVAL, 80, 'São José', null, 'Parnaíba', 'Rua Santa Catarina', 2813, 'PI', 69334233, 'Brasil');
