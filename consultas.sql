1ca08d4efbf60ee76d95c98c3627ddb18a16dbea
Danilo Silva <49529180+JDaniloC@users.noreply.github.com>
2022-04-12 10:21:33 -0300

    Update IN and FULL OUTER JOIN
    Replace IN by FULL OUTER JOIN and add a new FULL OUTER JOIN

--- consultas.sql
+++ consultas.sql
@@ -12,9 +12,10 @@ UPDATE agendamento SET id_centro = (
 CREATE INDEX indice_nome ON Pessoa(nome);
 
 
--- 3 INSERT INTO: Povoa as tabelas
-INSERT INTO Pessoa VALUES ('74846020940', 'Agatha Tânia da Cunha', 'F', 
-                           TO_DATE('23/03/1987', 'dd/mm/yyyy'));
+-- 3 INSERT INTO: Adiciona uma nova consulta
+INSERT INTO Agendamento VALUES ('48466958401', TO_DATE('01/03/2022', 'dd/mm/yyyy'), 
+                                TO_DATE('07/03/2022', 'dd/mm/yyyy'), '72376837219', '25483157036', 
+                                'Consulta sobre uma mordida de gato no pulso.', 3);
 
 -- 4 UPDATE: Troca o número de uma pessoa
 UPDATE Telefone_pessoa
@@ -30,15 +31,19 @@ WHERE resultado LIKE '%Tumor cerebral%');
 SELECT cpf_profissional, cpf_paciente, finalidade, hora_marcacao 
     FROM Agendamento WHERE hora_marcacao >= TO_DATE('2021-01-01','YYYY-MM-DD');
 
--- 7 BETWEEN: Seleciona um paciente do sexo feminino em que a data de nascimento dela seja entre 01/01/1960 e 01/01/1970
+-- 7 BETWEEN: Seleciona um paciente do sexo feminino em que a data de nascimento dela seja década de 60
 SELECT cpf_paciente, nome, sexo, data_de_nascimento FROM Paciente, Pessoa
     WHERE Paciente.cpf_paciente = Pessoa.cpf_pessoa AND sexo = 'F' AND
     data_de_nascimento BETWEEN
     TO_DATE('1960-01-01', 'YYYY-MM-DD') AND TO_DATE('1970-01-01', 'YYYY-MM-DD');
 
-
--- 8 IN
-
+-- 8 IN: Exibe todos os profissionais de saúde do centro (1, 3)
+SELECT cpf_atendente, Atendente.centro AS centro_do_atendente,
+    cpf_profissional, Profissional_de_saude.centro AS centro_do_profissional
+FROM Profissional_de_saude
+    FULL OUTER JOIN Atendente ON Atendente.cpf_atendente = Profissional_de_saude.cpf_profissional
+WHERE Atendente.centro IN (1, 3) OR Profissional_de_saude.centro IN (1, 3)
+ORDER BY centro_do_atendente, centro_do_profissional;
 
 -- 9 LIKE: Exibe os agendamentos das pessoas que representam a média, das quais tem como finalidade o exame de raio-x
 SELECT ROUND(AVG(Trunc((SYSDATE - Pessoa.data_de_nascimento)/365, 0))) AS idade_media
@@ -48,16 +53,16 @@ WHERE finalidade LIKE '%Raio-x%';
 -- 10 IS NULL ou IS NOT NULL: Atualiza todos endereços que não tem complemento para "Endereço único"
 UPDATE Endereco_centro SET complemento = 'Endereço único' WHERE complemento IS NULL;
 
--- 11 INNER JOIN: Une o pacinete e seu respectivo paciente
+-- 11 INNER JOIN: Une a tabela pessoa e seu respectivo paciente
 SELECT Pessoa.nome, Paciente.cpf_paciente, Agendamento.hora_agendamento, Agendamento.finalidade 
     FROM Paciente INNER JOIN Pessoa ON Paciente.cpf_paciente = Pessoa.cpf_pessoa 
     INNER JOIN Agendamento ON Paciente.cpf_paciente = Agendamento.cpf_paciente;
 
--- 12 MAX: Seleciona a capacidade máxima de cada centro
+-- 12 MAX: Seleciona a capacidade máxima dentre os centros
 SELECT MAX(capacidade)
 FROM Centro;
 
--- 13 MIN: Seleciona a capacidade mínima de cada centro
+-- 13 MIN: Seleciona a capacidade mínima dentre os centro
 SELECT MIN(capacidade)
 FROM Centro;
 
@@ -72,14 +77,13 @@ SELECT Centro.id_centro, COUNT(cpf_atendente) FROM Centro, Agendamento
     AND TO_TIMESTAMP('01/07/2020 01:00:00', 'DD/MM/YYYY HH24:MI:SS')
     GROUP BY Centro.id_centro;
 
--- 16 LEFT ou RIGHT ou FULL OUTER JOIN: Exibe todos os profissionais de saúde do centro (1, 3)
-SELECT cpf_atendente, cpf_profissional,
-    Atendente.centro AS centro_do_atendente,
-    Profissional_de_saude.centro AS centro_do_profissional
-FROM Profissional_de_saude
-    FULL OUTER JOIN Atendente ON Atendente.cpf_atendente = Profissional_de_saude.cpf_profissional
-WHERE Atendente.centro IN (1, 3) OR Profissional_de_saude.centro IN (1, 3)
-ORDER BY centro_do_atendente, centro_do_profissional;
+-- 16 LEFT ou RIGHT ou FULL OUTER JOIN: Exibe os gerentes e gerenciados
+SELECT pessoa_gerente.nome gerente, pessoa_gerenciado.nome gerenciado FROM Profissional_de_saude gerente
+    FULL OUTER JOIN Profissional_de_saude gerenciado ON gerenciado.cpf_gerente = gerente.cpf_profissional
+    LEFT JOIN Pessoa pessoa_gerente ON pessoa_gerente.cpf_pessoa = gerenciado.cpf_gerente
+    LEFT JOIN Pessoa pessoa_gerenciado ON pessoa_gerenciado.cpf_pessoa = gerenciado.cpf_profissional
+WHERE pessoa_gerente.nome IS NOT NULL OR pessoa_gerenciado.nome IS NOT NULL
+ORDER BY gerente, gerenciado;
 
 -- 17 SUBCONSULTA COM OPERADOR RELACIONAL: 
 
