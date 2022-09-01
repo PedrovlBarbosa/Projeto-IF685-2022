e49387f3d8576fcd73670a87489abc4563bb6295
PedrovlBarbosa <55454762+PedrovlBarbosa@users.noreply.github.com>
2022-08-31 22:56:32 -0300

    Update inserts.sql

--- alugueis/SQL/relacional/inserts.sql
+++ alugueis/SQL/relacional/inserts.sql
@@ -13,9 +13,9 @@ TRUNCATE TABLE Endereco;
 
 
 INSERT INTO Endereco VALUES ('04302021', 'APTO', 'Parque Imperial', 'Rua Paracatu', 'São Paulo', 'SP'),
-                            ('20040002', 'CASA', 'Centro', 'Avenida Rio Branco', 'Rio de Janeiro', 'RJ'),
-                            ('75802095', 'CASA', 'Vila Jardim Rio Claro', 'Avenida Tocantins', 'Jataí', 'GO'),
-                            ('03962040', 'APTO', 'Cidade São Mateus', 'Rua Maria Luísa do Val Penteado', 'São Paulo', 'SP'),
+INSERT INTO Endereco VALUES ('20040002', 'CASA', 'Centro', 'Avenida Rio Branco', 'Rio de Janeiro', 'RJ'),
+INSERT INTO Endereco VALUES ('75802095', 'CASA', 'Vila Jardim Rio Claro', 'Avenida Tocantins', 'Jataí', 'GO'),
+INSERT INTO Endereco VALUES ('03962040', 'APTO', 'Cidade São Mateus', 'Rua Maria Luísa do Val Penteado', 'São Paulo', 'SP'),
                             ('14401150', 'APTO', 'Cidade Nova', 'Rua Frederico Moura', 'Franca', 'SP'),
                             ('64000290', 'APTO', 'Centro', 'Rua Arlindo Nogueira', 'Teresina', 'PI'),
                             ('76900032', 'CASA', 'Centro', 'Travessa da CDL', 'Ji-Paraná', 'RO'),
@@ -41,12 +41,14 @@ INSERT INTO Endereco VALUES ('04302021', 'APTO', 'Parque Imperial', 'Rua Paracat
                             ('29141765', 'APTO', 'Rio Marinho', 'Rua Orquídea', 'Cariacica', 'ES'),
                             ('68376315', 'CASA', 'Colinas', 'Rua WE Cinco', 'Altamira', 'PA'), 
                             ('62011140', 'APTO', 'Centro', 'Rua Domingos Olímpio', 'Sobral', 'CE');
+                            ('29050545', 'EMPRESARIAL', 'Enseada do Suá', 'Rua José Alexandre Buaiz', 'Vitoria', 'ES');
+                            ('90480000', 'EMPRESARIAL', 'Boa Vista', 'Avenida Carlos Gomes', 'Porto Alegre', 'RS');
 
 
 
 
 
-INSERT INTO Pessoa VALUES ('261.319.150-32', 'Frederico', 'M', '10/20/200', '04302021'),
+INSERT INTO Pessoa VALUES ('261.319.150-32', 'Frederico', 'M', TO_DATE('10/20/2001', 'mm/dd/yyyy'), '04302021'),
                           ('140.067.983-45', 'Amanda', 'F', '14/01/1998', '20040002'),
                           ('093.010.763-78', 'Hélio', 'M', '10/04/2002', '75802095'),
                           ('654.312.123-66', 'Carla', 'F', '10/11/1990', '03962040'),
@@ -113,103 +115,86 @@ INSERT INTO Corretor VALUES ('261.319.150-32', '10/15/2021', 2000, '970.465.370-
                             ('602.312.980-30', '02/25/2020', 1800, '970.465.370-03'),
                             ('970.465.370-03', '08/06/2019', 3450, '093.010.763-78');
 
-INSERT INTO Cliente VALUES  ('552.010.750-53'), 
-                            ('103.320.160-05'),
-                            ('652.730.540-01'),
-                            ('302.292.330-90'),
-                            ('481.531.180-30'),
-                            ('123.419.870-36'),
-                            ('738.529.010-81'),
-                            ('804.759.740-00'),
-                            ('019.588.030-73'),
-                            ('169.161.620-66'),
-                            ('053.428.500-70'),
-                            ('146.000.310-16'),
-                            ('522.046.960-64'),
-                            ('222.816.320-16'),
-                            ('668.011.180-30'),
-                            ('259.829.110-70'),
-                            ('379.840.440-23'),
-                            ('657.885.490-86'),
-                            ('521.907.640-09'),
-                            ('829.491.020-18'),
-                            ('091.665.070-73');
+INSERT INTO Cliente VALUES ('552.010.750-53'); 
+INSERT INTO Cliente VALUES ('103.320.160-05');
+INSERT INTO Cliente VALUES ('652.730.540-01');
+INSERT INTO Cliente VALUES ('302.292.330-90');
+INSERT INTO Cliente VALUES ('481.531.180-30');
+INSERT INTO Cliente VALUES ('123.419.870-36');
+INSERT INTO Cliente VALUES ('738.529.010-81');
+INSERT INTO Cliente VALUES ('804.759.740-00');
+INSERT INTO Cliente VALUES ('019.588.030-73');
+INSERT INTO Cliente VALUES ('169.161.620-66');
+INSERT INTO Cliente VALUES ('053.428.500-70');
+INSERT INTO Cliente VALUES ('146.000.310-16');
+INSERT INTO Cliente VALUES ('522.046.960-64');
+INSERT INTO Cliente VALUES ('222.816.320-16');
+INSERT INTO Cliente VALUES ('668.011.180-30');
+INSERT INTO Cliente VALUES ('259.829.110-70');
+INSERT INTO Cliente VALUES ('379.840.440-23');
+INSERT INTO Cliente VALUES ('657.885.490-86');
+INSERT INTO Cliente VALUES ('521.907.640-09');
+INSERT INTO Cliente VALUES ('829.491.020-18');
+INSERT INTO Cliente VALUES ('091.665.070-73');
 
-INSERT INTO Locador VALUES  ('552.010.750-53'),
-                            ('103.320.160-05'),
-                            ('652.730.540-01'),
-                            ('302.292.330-90'),
-                            ('481.531.180-30'),
-                            ('091.665.070-73'),
-                            ('123.419.870-36');
-
-
-INSERT INTO Locatario VALUES  ('738.529.010-81'),
-                              ('804.759.740-00'),
-                              ('019.588.030-73'),
-                              ('169.161.620-66'),
-                              ('053.428.500-70'),
-                              ('146.000.310-16'),
-                              ('522.046.960-64');
+INSERT INTO Locador VALUES ('552.010.750-53');
+INSERT INTO Locador VALUES ('103.320.160-05');
+INSERT INTO Locador VALUES ('652.730.540-01');
+INSERT INTO Locador VALUES ('302.292.330-90');
+INSERT INTO Locador VALUES ('481.531.180-30');
+INSERT INTO Locador VALUES ('091.665.070-73');
+INSERT INTO Locador VALUES ('123.419.870-36');
 
 
-INSERT INTO Fiador VALUES ('222.816.320-16'),
-                          ('668.011.180-30'),
-                          ('259.829.110-70'),
-                          ('379.840.440-23'),
-                          ('657.885.490-86'),
-                          ('521.907.640-09'),
-                          ('829.491.020-18');
-
-INSERT INTO Sala VALUES (sala_seq.NEXTVAL, 30.50, '96204040', '261.319.150-32', '552.010.750-53'),
-                        (sala_seq.NEXTVAL, 50.80, '06709015', '140.067.983-45', '103.320.160-05'),
-                        (sala_seq.NEXTVAL, 44.00, '80520560', '093.010.763-78', '652.730.540-01'),
-                        (sala_seq.NEXTVAL, 72.00, '71020631', '654.312.123-66', '302.292.330-90'),
-                        (sala_seq.NEXTVAL, 44.00, '72347856', '000.000.000-00', '140.067.983-45'),
-                        (sala_seq.NEXTVAL, 44.00, '72347856', '000.000.000-00', '140.067.983-45'),
-                        (sala_seq.NEXTVAL, 44.00, '72347856', '000.000.000-00', '140.067.983-45');
+INSERT INTO Locatario VALUES ('738.529.010-81');
+INSERT INTO Locatario VALUES ('804.759.740-00');
+INSERT INTO Locatario VALUES ('019.588.030-73');
+INSERT INTO Locatario VALUES ('169.161.620-66');
+INSERT INTO Locatario VALUES ('053.428.500-70');
+INSERT INTO Locatario VALUES ('146.000.310-16');
+INSERT INTO Locatario VALUES ('522.046.960-64');
 
 
-
-INSERT INTO Avaliacao VALUES('000.000.000-00', '140.067.983-45', 2, 565.59);
--- CREATE TABLE Avaliacao (
--- 	cpf_corretor VARCHAR2(14),
---     cpf_locador VARCHAR2(14),
--- 	codigo_sala INTEGER,
--- 	preco_unit NUMBER(3, 2),
-
---     CONSTRAINT codigo_sala_avaliacao_fkey FOREIGN KEY(codigo_sala, cpf_locador) REFERENCES Sala(codigo, cpf_dono),
--- 	CONSTRAINT cpf_locador_avaliacao_fkey FOREIGN KEY(cpf_locador) REFERENCES Locador(cpf_locador),
--- 	CONSTRAINT cpf_corretor_avaliacao_fkey FOREIGN KEY(cpf_corretor) REFERENCES Corretor(cpf_corretor),
--- 	CONSTRAINT avaliacao_pkey PRIMARY KEY(codigo_sala, cpf_locador, cpf_corretor)
--- );
+INSERT INTO Fiador VALUES ('222.816.320-16');
+INSERT INTO Fiador VALUES ('668.011.180-30');
+INSERT INTO Fiador VALUES ('259.829.110-70');
+INSERT INTO Fiador VALUES ('379.840.440-23');
+INSERT INTO Fiador VALUES ('657.885.490-86');
+INSERT INTO Fiador VALUES ('521.907.640-09');
+INSERT INTO Fiador VALUES ('829.491.020-18');
 
 
-INSERT INTO lucro VALUES(60.00, 40.00),
-                        (55.00, 55.00),
-                        (70.00, 30.00),
-                        (85.00, 15.00),
-                        (65.00, 35.00),
-                        (80.00, 20.00),
-                        (90.00, 10.00);
+INSERT INTO Sala VALUES (sala_seq.NEXTVAL, 30.50, '96204040', '261.319.150-32', '552.010.750-53');
+INSERT INTO Sala VALUES (sala_seq.NEXTVAL, 50.80, '06709015', '140.067.983-45', '103.320.160-05');
+INSERT INTO Sala VALUES (sala_seq.NEXTVAL, 44.00, '80520560', '093.010.763-78', '652.730.540-01');
+INSERT INTO Sala VALUES (sala_seq.NEXTVAL, 72.00, '71020631', '654.312.123-66', '302.292.330-90');
+INSERT INTO Sala VALUES (sala_seq.NEXTVAL, 44.00, '51020280', '948.695.710-05', '481.531.180-30');
+INSERT INTO Sala VALUES (sala_seq.NEXTVAL, 69.69, '51010000', '602.312.980-30', '091.665.070-73');
+INSERT INTO Sala VALUES (sala_seq.NEXTVAL, 80.00, '29050545', '970.465.370-03', '123.419.870-36');
+INSERT INTO Sala VALUES (sala_seq.NEXTVAL, 33.60, '90480000', '261.319.150-32', '123.419.870-36');
 
 
-INSERT INTO Aluguel VALUES
-  ('093.010.763-78', '140.067.983-45', '654.312.123-66', 2, '2022-10-18', '2023-01-01', 60.00);
--- CREATE TABLE Aluguel (
---   	cpf_locatario VARCHAR2(14),
---   	cpf_dono VARCHAR2(14),
---   	cpf_fiador VARCHAR2(14),
---   	codigo_sala INTEGER,
---   	data_inicio DATE,
---   	data_final DATE,
---   	porc_locador NUMBER(2, 2),
+INSERT INTO Avaliacao VALUES ('261.319.150-32', '552.010.750-53', 1, 25.20);
+INSERT INTO Avaliacao VALUES ('140.067.983-45', '103.320.160-05', 2, 64.56);
+INSERT INTO Avaliacao VALUES ('093.010.763-78', '652.730.540-01', 3, 36.09);
+INSERT INTO Avaliacao VALUES ('654.312.123-66', '302.292.330-90', 4, 56.00);
+INSERT INTO Avaliacao VALUES ('948.695.710-05', '481.531.180-30', 5, 26.99);
+INSERT INTO Avaliacao VALUES ('602.312.980-30', '091.665.070-73', 6, 55.39);
+INSERT INTO Avaliacao VALUES ('970.465.370-03', '123.419.870-36', 7, 65.59);
+INSERT INTO Avaliacao VALUES ('261.319.150-32', '123.419.870-36', 8, 28.63);
 
--- 	CONSTRAINT alguel_pkey PRIMARY KEY(codigo_sala, cpf_locatario, cpf_dono, data_inicio),	
--- 	CONSTRAINT porc_locador_fkey FOREIGN KEY (porc_locador) REFERENCES Lucro(porc_locador),
--- 	CONSTRAINT cpf_dono_aluguel_fkey FOREIGN KEY (cpf_dono) REFERENCES Locador(cpf_locador),
--- 	CONSTRAINT cpf_fiador_aluguel_fkey FOREIGN KEY (cpf_fiador) REFERENCES Fiador(cpf_fiador),
--- 	CONSTRAINT cpf_locatario_aluguel_fkey FOREIGN KEY(cpf_locatario) REFERENCES Locatario(cpf_locatario),
--- 	CONSTRAINT codigo_sala_aluguel_fkey FOREIGN KEY (codigo_sala, cpf_dono) REFERENCES Sala(codigo, cpf_dono)
--- );
+
+INSERT INTO lucro VALUES (60.00, 40.00);
+INSERT INTO lucro VALUES (50.00, 50.00);
+INSERT INTO lucro VALUES (70.00, 30.00);
+INSERT INTO lucro VALUES (85.00, 15.00);
+INSERT INTO lucro VALUES (65.00, 35.00);
+INSERT INTO lucro VALUES (80.00, 20.00);
+INSERT INTO lucro VALUES (90.00, 10.00);
+
+
+INSERT INTO Aluguel VALUES ('738.529.010-81', '103.320.160-05', '222.816.320-16', 2, TO_DATE ('18-10-2022','dd/mm/yyyy'), TO_DATE ('18-10-2023','dd/mm/yyyy'), 60.00);
+INSERT INTO Aluguel VALUES ('804.759.740-00', '302.292.330-90', '668.011.180-30', 4, TO_DATE ('16-08-2022','dd/mm/yyyy'), TO_DATE ('16-08-2024','dd/mm/yyyy'), 65.00);
+INSERT INTO Aluguel VALUES ('169.161.620-66', '481.531.180-30', '379.840.440-23', 5, TO_DATE ('06-09-2022','dd/mm/yyyy'), TO_DATE ('06-03-2023','dd/mm/yyyy'), 85.00);
+INSERT INTO Aluguel VALUES ('522.046.960-64', '123.419.870-36', '829.491.020-18', 7, TO_DATE ('25-01-2022','dd/mm/yyyy'), TO_DATE ('26-02-2022','dd/mm/yyyy'), 50.00);
 
